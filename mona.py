fcba69da97fe5e5ffd026bcf5a6b45dca6da945c
Ninzaninja1 <video.ninzaninja@gmail.com>
2026-03-22 10:26:26 +0000

    pasted grid code into mona

--- mona.py
+++ mona.py
@@ -1,6 +1,27 @@
 import os
 os.environ["QT_QPA_PLATFORM"] = "xcb" 
 import cv2
+import numpy as np
+
+def draw_grid(img, grid_shape, color=(0, 0, 255), thickness=1):
+    h, w, _ = img.shape
+    rows, cols = grid_shape
+    dy, dx = h / rows, w / cols
+
+    # draw vertical lines
+    for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+        x = int(round(x))
+        cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
+
+    # draw horizontal lines
+    for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+        y = int(round(y))
+        cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
+
+    return img
+import os
+os.environ["QT_QPA_PLATFORM"] = "xcb" 
+import cv2
 
 web = cv2.VideoCapture(0)
 
